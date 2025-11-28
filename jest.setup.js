4d2f4401817df40ad9f484b390bded6450998ca2
zeynnaz06 <s230307020@ankarabilim.edu.tr>
2025-11-28 18:50:01 +0300

    Revert Jest setup: remove test scripts, deps and test files; restore App.tsx

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// Jest setup for React Native Testing Library
-require('@testing-library/jest-native/extend-expect');
-
-// Silence native animated warnings
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
