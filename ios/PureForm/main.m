21ec424f03751b8344858da299e234b1c6ed84e3
inankaratas35 <inan3524@gmail.com>
2025-12-17 00:49:09 +0300

    son

--- ios/PureForm/main.m
+++ ios/PureForm/main.m
@@ -2,8 +2,14 @@
 
 #import "AppDelegate.h"
 
+static void HandleUncaughtException(NSException *exception) {
+  NSLog(@"[Boot] Uncaught exception: %@\nStack:\n%@", exception, [exception callStackSymbols]);
+}
+
 int main(int argc, char * argv[]) {
   @autoreleasepool {
+    NSSetUncaughtExceptionHandler(&HandleUncaughtException);
+    NSLog(@"[Boot] main() starting. AppDelegate class: %@", NSStringFromClass([AppDelegate class]));
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
